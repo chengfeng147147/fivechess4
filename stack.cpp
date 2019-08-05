ea0c12592fc155ab547d0ebcbdd99021f7eab0df
chengfeng <chengfeng147147@gmail.com>
2019-08-05 13:43:34 +0800

    change name

--- stack.cpp
+++ stack.cpp
@@ -3,26 +3,26 @@
 
 struct stepStack{
 		Step step[MAXCOUNT];
-		int stack_pointer;
+		int stack_lastpointer;
 		int stack_firstpointer;
 }step_stack;
 
 void stack_init(){
-	step_stack.stack_pointer = 0;
+	step_stack.stack_lastpointer = 0;
 	step_stack.stack_firstpointer=0;
 }
 void stack_push(Step* step){
-	step_stack.step[step_stack.stack_pointer].x = step->x;
-	step_stack.step[step_stack.stack_pointer].y = step->y;
-	step_stack.step[step_stack.stack_pointer].colour = step->colour;
-	step_stack.stack_pointer++;
+	step_stack.step[step_stack.stack_lastpointer].x = step->x;
+	step_stack.step[step_stack.stack_lastpointer].y = step->y;
+	step_stack.step[step_stack.stack_lastpointer].colour = step->colour;
+	step_stack.stack_lastpointer++;
 }
 
 void stack_pop(Step* step){
-	step->x = step_stack.step[step_stack.stack_pointer-1].x;
-	step->y = step_stack.step[step_stack.stack_pointer-1].y;
-	step->colour = step_stack.step[step_stack.stack_pointer-1].colour;
-	step_stack.stack_pointer--;
+	step->x = step_stack.step[step_stack.stack_lastpointer-1].x;
+	step->y = step_stack.step[step_stack.stack_lastpointer-1].y;
+	step->colour = step_stack.step[step_stack.stack_lastpointer-1].colour;
+	step_stack.stack_lastpointer--;
 }
 void stack_firstpop(Step* step){
 	step->x = step_stack.step[step_stack.stack_firstpointer].x;
